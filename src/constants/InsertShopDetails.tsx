c7104a8177b69031aea384a22e4bfc0883859a88
Vrushabh <vrushabhgawas14@gmail.com>
2024-12-13 23:41:43 +0530

    Data inserted

--- src/constants/InsertShopDetails.tsx
+++ src/constants/InsertShopDetails.tsx
@@ -1,8 +1,8 @@
 export const InsertShopDetails = [
   {
-    name: "Shop 1",
-    category: "Food",
-    slug: "shop1",
+    name: "Live Ice Cream by Harah",
+    category: "Ice Cream",
+    slug: "live-ice-cream",
     imageURL:
       "https://drive.google.com/uc?id=1bUieKLO86OLZTEOjNiZg4q2ID-n4gKgn",
     rating: 0,
@@ -39,14 +39,14 @@ export const InsertShopDetails = [
       },
     ],
     startDate: 14,
-    endDate: 13,
+    endDate: 20,
     ratedUsers: [],
     ratingsArray: [],
   },
   {
-    name: "Shop 2",
-    category: "Food",
-    slug: "shop2",
+    name: "Panipuri Corner by Sneha",
+    category: "Panipuri",
+    slug: "panipuri-corner",
     imageURL:
       "https://drive.google.com/uc?id=1bUieKLO86OLZTEOjNiZg4q2ID-n4gKgn",
     rating: 0,
@@ -71,52 +71,286 @@ export const InsertShopDetails = [
       },
     ],
     startDate: 14,
-    endDate: 12,
+    endDate: 20,
     ratedUsers: [],
     ratingsArray: [],
   },
   {
-    name: "Shop 3",
+    name: "Little Bites by Nidhi",
+    category: "Food",
+    slug: "little-bites",
+    imageURL:
+      "https://drive.google.com/uc?id=1bUieKLO86OLZTEOjNiZg4q2ID-n4gKgn",
+    rating: 0,
+    products: [
+      {
+        name: "Product 2",
+        price: 44,
+        productImage:
+          "https://drive.google.com/uc?id=1ckWntpoawUpqMKU0vStswz4k3SlOEQH2",
+      },
+      {
+        name: "Product 3",
+        price: 20,
+        productImage:
+          "https://drive.google.com/uc?id=1ckWntpoawUpqMKU0vStswz4k3SlOEQH2",
+      },
+      {
+        name: "Product 4",
+        price: 35,
+        productImage:
+          "https://drive.google.com/uc?id=1ckWntpoawUpqMKU0vStswz4k3SlOEQH2",
+      },
+      {
+        name: "Product 5",
+        price: 70,
+        productImage:
+          "https://drive.google.com/uc?id=1ckWntpoawUpqMKU0vStswz4k3SlOEQH2",
+      },
+    ],
+    startDate: 14,
+    endDate: 20,
+    ratedUsers: [],
+    ratingsArray: [],
+  },
+  {
+    name: "Chills by Kshitij",
+    category: "Food",
+    slug: "chills",
+    imageURL:
+      "https://drive.google.com/uc?id=1bUieKLO86OLZTEOjNiZg4q2ID-n4gKgn",
+    rating: 0,
+    products: [
+      {
+        name: "Product 2",
+        price: 44,
+        productImage:
+          "https://drive.google.com/uc?id=1ckWntpoawUpqMKU0vStswz4k3SlOEQH2",
+      },
+      {
+        name: "Product 3",
+        price: 20,
+        productImage:
+          "https://drive.google.com/uc?id=1ckWntpoawUpqMKU0vStswz4k3SlOEQH2",
+      },
+    ],
+    startDate: 14,
+    endDate: 20,
+    ratedUsers: [],
+    ratingsArray: [],
+  },
+  {
+    name: "Cold Beverages by Sumit",
+    category: "Beverages",
+    slug: "cold-beverages",
+    imageURL:
+      "https://drive.google.com/uc?id=1bUieKLO86OLZTEOjNiZg4q2ID-n4gKgn",
+    rating: 0,
+    products: [
+      {
+        name: "Product 2",
+        price: 44,
+        productImage:
+          "https://drive.google.com/uc?id=1ckWntpoawUpqMKU0vStswz4k3SlOEQH2",
+      },
+      {
+        name: "Product 3",
+        price: 20,
+        productImage:
+          "https://drive.google.com/uc?id=1ckWntpoawUpqMKU0vStswz4k3SlOEQH2",
+      },
+    ],
+    startDate: 14,
+    endDate: 20,
+    ratedUsers: [],
+    ratingsArray: [],
+  },
+  {
+    name: "Beverages by Vyom",
+    category: "Beverages",
+    slug: "beverages",
+    imageURL:
+      "https://drive.google.com/uc?id=1bUieKLO86OLZTEOjNiZg4q2ID-n4gKgn",
+    rating: 0,
+    products: [
+      {
+        name: "Product 2",
+        price: 44,
+        productImage:
+          "https://drive.google.com/uc?id=1ckWntpoawUpqMKU0vStswz4k3SlOEQH2",
+      },
+      {
+        name: "Product 3",
+        price: 20,
+        productImage:
+          "https://drive.google.com/uc?id=1ckWntpoawUpqMKU0vStswz4k3SlOEQH2",
+      },
+    ],
+    startDate: 14,
+    endDate: 20,
+    ratedUsers: [],
+    ratingsArray: [],
+  },
+  {
+    name: "Ice Cream Mania by Jenam",
+    category: "Ice Cream",
+    slug: "ice-cream-mania",
+    imageURL:
+      "https://drive.google.com/uc?id=1bUieKLO86OLZTEOjNiZg4q2ID-n4gKgn",
+    rating: 0,
+    products: [
+      {
+        name: "Product 2",
+        price: 44,
+        productImage:
+          "https://drive.google.com/uc?id=1ckWntpoawUpqMKU0vStswz4k3SlOEQH2",
+      },
+      {
+        name: "Product 3",
+        price: 20,
+        productImage:
+          "https://drive.google.com/uc?id=1ckWntpoawUpqMKU0vStswz4k3SlOEQH2",
+      },
+    ],
+    startDate: 14,
+    endDate: 20,
+    ratedUsers: [],
+    ratingsArray: [],
+  },
+  {
+    name: "Snack Attack by Chinmay",
+    category: "Food",
+    slug: "snack-attack",
+    imageURL:
+      "https://drive.google.com/uc?id=1bUieKLO86OLZTEOjNiZg4q2ID-n4gKgn",
+    rating: 0,
+    products: [
+      {
+        name: "Product 2",
+        price: 44,
+        productImage:
+          "https://drive.google.com/uc?id=1ckWntpoawUpqMKU0vStswz4k3SlOEQH2",
+      },
+      {
+        name: "Product 3",
+        price: 20,
+        productImage:
+          "https://drive.google.com/uc?id=1ckWntpoawUpqMKU0vStswz4k3SlOEQH2",
+      },
+    ],
+    startDate: 14,
+    endDate: 19,
+    ratedUsers: [],
+    ratingsArray: [],
+  },
+  {
+    name: "Mocktails by Dheeraj",
+    category: "Food",
+    slug: "mocktails",
+    imageURL:
+      "https://drive.google.com/uc?id=1bUieKLO86OLZTEOjNiZg4q2ID-n4gKgn",
+    rating: 0,
+    products: [
+      {
+        name: "Product 2",
+        price: 44,
+        productImage:
+          "https://drive.google.com/uc?id=1ckWntpoawUpqMKU0vStswz4k3SlOEQH2",
+      },
+      {
+        name: "Product 3",
+        price: 20,
+        productImage:
+          "https://drive.google.com/uc?id=1ckWntpoawUpqMKU0vStswz4k3SlOEQH2",
+      },
+    ],
+    startDate: 14,
+    endDate: 14,
+    ratedUsers: [],
+    ratingsArray: [],
+  },
+  {
+    name: "Pinterestry by Sai",
+    category: "styles",
+    slug: "pinterestry",
+    imageURL:
+      "https://drive.google.com/uc?id=1bUieKLO86OLZTEOjNiZg4q2ID-n4gKgn",
+    rating: 0,
+    products: [
+      {
+        name: "Product 2",
+        price: 44,
+        productImage:
+          "https://drive.google.com/uc?id=1ckWntpoawUpqMKU0vStswz4k3SlOEQH2",
+      },
+      {
+        name: "Product 3",
+        price: 20,
+        productImage:
+          "https://drive.google.com/uc?id=1ckWntpoawUpqMKU0vStswz4k3SlOEQH2",
+      },
+    ],
+    startDate: 14,
+    endDate: 16,
+    ratedUsers: [],
+    ratingsArray: [],
+  },
+  {
+    name: "Totally Totes by Arya",
+    category: "Bags",
+    slug: "totally-totes",
+    imageURL:
+      "https://drive.google.com/uc?id=1bUieKLO86OLZTEOjNiZg4q2ID-n4gKgn",
+    rating: 0,
+    products: [
+      {
+        name: "Product 2",
+        price: 44,
+        productImage:
+          "https://drive.google.com/uc?id=1ckWntpoawUpqMKU0vStswz4k3SlOEQH2",
+      },
+      {
+        name: "Product 3",
+        price: 20,
+        productImage:
+          "https://drive.google.com/uc?id=1ckWntpoawUpqMKU0vStswz4k3SlOEQH2",
+      },
+    ],
+    startDate: 14,
+    endDate: 16,
+    ratedUsers: [],
+    ratingsArray: [],
+  },
+  {
+    name: "ADHD by Arya",
+    category: "Food",
+    slug: "adhd",
+    imageURL:
+      "https://drive.google.com/uc?id=1bUieKLO86OLZTEOjNiZg4q2ID-n4gKgn",
+    rating: 0,
+    products: [
+      {
+        name: "Product 2",
+        price: 44,
+        productImage:
+          "https://drive.google.com/uc?id=1ckWntpoawUpqMKU0vStswz4k3SlOEQH2",
+      },
+      {
+        name: "Product 3",
+        price: 20,
+        productImage:
+          "https://drive.google.com/uc?id=1ckWntpoawUpqMKU0vStswz4k3SlOEQH2",
+      },
+    ],
+    startDate: 14,
+    endDate: 16,
+    ratedUsers: [],
+    ratingsArray: [],
+  },
+  {
+    name: "Book Stall by Vidhi",
     category: "Books",
-    slug: "shop3",
-    imageURL:
-      "https://drive.google.com/uc?id=1bUieKLO86OLZTEOjNiZg4q2ID-n4gKgn",
-    rating: 0,
-    products: [
-      {
-        name: "Product 2",
-        price: 44,
-        productImage:
-          "https://drive.google.com/uc?id=1ckWntpoawUpqMKU0vStswz4k3SlOEQH2",
-      },
-      {
-        name: "Product 3",
-        price: 20,
-        productImage:
-          "https://drive.google.com/uc?id=1ckWntpoawUpqMKU0vStswz4k3SlOEQH2",
-      },
-      {
-        name: "Product 4",
-        price: 35,
-        productImage:
-          "https://drive.google.com/uc?id=1ckWntpoawUpqMKU0vStswz4k3SlOEQH2",
-      },
-      {
-        name: "Product 5",
-        price: 70,
-        productImage:
-          "https://drive.google.com/uc?id=1ckWntpoawUpqMKU0vStswz4k3SlOEQH2",
-      },
-    ],
-    startDate: 14,
-    endDate: 17,
-    ratedUsers: [],
-    ratingsArray: [],
-  },
-  {
-    name: "Shop 4",
-    category: "Clothes",
-    slug: "shop4",
+    slug: "book-stall",
     imageURL:
       "https://drive.google.com/uc?id=1bUieKLO86OLZTEOjNiZg4q2ID-n4gKgn",
     rating: 0,
@@ -135,7 +369,7 @@ export const InsertShopDetails = [
       },
     ],
     startDate: 14,
-    endDate: 17,
+    endDate: 14,
     ratedUsers: [],
     ratingsArray: [],
   },
